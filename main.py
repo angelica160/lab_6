a83e5f3cfb1f3144dc287c7835ffa0fdc03b190a
angelicahanley <hanleyangelica@ufl.edu>
2023-10-25 12:30:25 -0400

    test

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #main function that prints the menu and asks the user for input
+def encode():
+    global newpassword
+    global password
+    new_password = ""
+    password = input("Please enter your password to encode:")
+    print("Your password has been encoded and stored!")
+    print("\n")
+    newpassword = ""
+    for digit in password:
+        newdigit = int(digit) + 3
+        newpassword = newpassword + str(newdigit)
+    return new_password
+def decode(newpassword):
+    pass
+
 def main():
     while input != 3:
         print("Menu")
@@ -8,20 +23,50 @@ def main():
         option = int(input("Please enter an option: "))
         #enocdes the inputted password, by adding 3 to each digit
         if option == 1:
-            password = input("Please enter your password to encode:")
-            print("Your password has been encoded and stored!")
-            print("\n")
-            newpassword = ""
-            for digit in password:
-                newdigit = int(digit)+3
-                newpassword = newpassword + str(newdigit)
+            encode()
         #where the decode function needed to be added
         elif option == 2:
-            pass
+            decode(newpassword)
         elif option == 3:
             break
 if __name__ == "__main__":
     main()
+
+
+"""
+#main function that prints the menu and asks the user for input
+def encode():
+    global new_password
+    global password
+    password = input("Please enter your password to encode:")
+    print("Your password has been encoded and stored!")
+    print("\n")
+    newpassword = ""
+    for digit in password:
+        newdigit = int(digit) + 3
+        newpassword = newpassword + str(newdigit)
+    return new_password
+def decode(new_password):
+    pass
+
+def main():
+    while input != 3:
+        print("Menu")
+        print("-" * 13)
+        print("1. Encode", "2. Decode", "3. Quit", sep="\n")
+        print("\n")
+        option = int(input("Please enter an option: "))
+        #enocdes the inputted password, by adding 3 to each digit
+        if option == 1:
+            encode()
+        #where the decode function needed to be added
+        elif option == 2:
+            decode(new_password)
+        elif option == 3:
+            break
+if __name__ == "__main__":
+    main()
+    """
 
 
 
